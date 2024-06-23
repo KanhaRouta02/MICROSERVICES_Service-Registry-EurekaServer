18d6d64169fe74fbf7e1ee868ed04207e61557ec
Kanha Routa <147900871+KanhaRouta02@users.noreply.github.com>
2024-06-23 20:02:11 +0530

    second commit

--- src/main/java/in/kanha/Application.java
+++ src/main/java/in/kanha/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// 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
+
 @SpringBootApplication
 @EnableEurekaServer
 public class Application {
